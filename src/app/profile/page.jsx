1cdac7052a71ff2fdc78aef6b6c74aee20587e0e
Juanies <juanromeroviudez@gmail.com>
2024-06-01 18:23:36 +0200

    Login form view

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,12 +1,20 @@
-import Button from '@/app/ui/buttons'
+import Button from '@/app/ui/optionslogin'
+import Input from '@/app/ui/input'
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen gap-2 p-24">
+    <main className="flex flex-col items-center justify-center min-h-screen gap-6 p-24">
       <div className='flex flex-col gap-2'>
         <Button type="google"/>
         <Button type="apple"/>
       </div>
+      <p>O continúa con tu correo electrónico</p>
+      <div className='flex flex-col gap-2'>
+        <Input type="username"/>
+        <Input type="email"/>
+        <Input type="password"/>
 
+      </div>
     </main>
   );
 }
