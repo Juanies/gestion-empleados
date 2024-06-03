8cd8d6eee077a092f1310fae6ef52a71e30d293d
Juanies <juanromeroviudez@gmail.com>
2024-06-03 12:50:25 +0200

    Encrypt cookie

--- src/app/actions/cookie.js
+++ src/app/actions/cookie.js
@@ -1,6 +1,47 @@
 'use server'
+import { SignJWT, jwtVerify } from 'jose';
+
 import { cookies } from 'next/headers';
 
-export async function setUsernameCookie(username) {
-  cookies().set('user', username, { httpOnly: true, secure: true, path: '/' });
+
+const secretKey = 'conunañlacontraseñaeslamasseguradelmundo'
+const key = new TextEncoder().encode(secretKey)
+
+export async function encrypt(payload){
+  return await new SignJWT(payload)
+  .setProtectedHeader({alg: 'HS256'})
+  .setIssuedAt()
+  .sign(key);
+}
+
+export async function decrypt(input) {
+  const { payload } = await jwtVerify(input, key, {
+    algorithms: ['HS256'],
+  });
+  return payload;
+}
+
+
+export async function login(username){
+
+  const session = await encrypt({username})
+
+   cookies().set('session', session, { httpOnly: true, secure: true, path: '/' });
+
+
+   getUser(session)
+
+  }
+
+
+export async function getUser(nameCookie){
+  const cookie = cookies().get('session')
+
+  if (cookie) {
+    const decryptedPayload = await decrypt(cookie.value);
+    console.log(decryptedPayload);
+    return decryptedPayload
+  } else {
+    throw new Error("No session cookie found")
+  }
 }
--- src/app/api/searchuser/route.js
+++ src/app/api/searchuser/route.js
@@ -1,6 +1,6 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
-import { setUsernameCookie } from '@/app/actions/cookie';
+import { login, setUsernameCookie } from '@/app/actions/cookie';
 
 export async function POST(request) {
   const { username, password } = await request.json();
@@ -9,11 +9,14 @@ export async function POST(request) {
     if (!username || !password) throw new Error('Username and password required');
 
     const result = await sql`SELECT * FROM usuario WHERE usuario = ${username} AND contraseña = ${password};`;
-    if (result.rowCount === 0) {
-      throw new Error('Invalid username or password');
+
+    if (result.rowCount >= 1) {
+      login(username)
+    }else{
+      throw new Error("Username or password failed")
     }
 
-    await setUsernameCookie(username);
+
 
     return NextResponse.json({ username }, { status: 200 });
   } catch (error) {
